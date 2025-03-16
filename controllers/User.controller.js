9b9b1aebc8fb2de64c0bfbfc69b204c4350464e4
Ophélia <ochheak@gmail.com>
2025-03-16 11:17:34 +0100

    signin done

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -24,3 +24,32 @@ exports.signup = async (req, res, next) => {
         next(createError(500, "Erreur lors de l'inscription", error.message));
     }
 };
+
+exports.signin = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ where: { email: req.body.email } });
+        if (!user) return next(createError(404, "Utilisateur non trouvé"));
+
+        //Vérification du mdp
+        const comparePassword = await bcrypt.compare(req.body.password, user.password)
+        if (!comparePassword) return next(createError(400, "Identifiants incorrects"));
+
+        // Génération du token
+        const token = jwt.sign({id: user.id}, ENV.TOKEN, {expiresIn : '24h'});
+
+        // On exclut le mdp du résultat user
+        const { password, ...userData } = user.dataValues;
+
+        // Création du cookie
+        res.cookie('access_token', token, {
+            httpOnly: true,
+            secure: false, // mettre à true pour https
+            sameSite: 'strict', // protège contre les attaques CSRF
+            maxAge: 24 * 60 * 60 * 1000 // 24h en millisecondes
+        })
+            .status(200)
+            .json(userData)
+    } catch (error) {
+        next(createError(500, "Erreur lors de la connexion", error.message));
+    }
+}
--- router/user.router.js
+++ router/user.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user.controller');
+const userController = require('../controllers/User.controller');
 
 router.post('/register', userController.signup);
+router.post('/login', userController.signin);
 
 module.exports = router;
