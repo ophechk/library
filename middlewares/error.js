be72f202d01d25650db5b6bead8977ffcc4e48ce
Ophélia <ochheak@gmail.com>
2025-03-16 21:12:21 +0100

    errorjs corrected

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,11 +1,10 @@
-// centraliser les erreur
+// centraliser les erreurs
 
 function createError(statusCode, message, details = null) {
-    const error = new Error(message);
-    error.statusCode = statusCode;
-    if (details) error.details = details;
-    return error;
-  }
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  if (details) error.details = details;
+  return error;
+}
 
-module.exports = createError();
-
+module.exports = createError; // Export the function itself
