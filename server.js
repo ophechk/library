58e6d481a2aebacb196535aed64800c4ce2786f5
Ophélia <ochheak@gmail.com>
2025-03-20 22:29:05 +0100

    modify acss and add new page gallery

--- server.js
+++ server.js
@@ -103,6 +103,26 @@ app.get('/books/:id', async (req, res) => {
     }
 });
 
+
+app.get('/gallery', (req, res) => {
+  // Exemple de données d'images
+  const images = [
+      { src: '/images/AscenseurVerre.jpg', caption: 'Charlie et le grand ascenseur de verre' },
+      { src: '/images/Chocolaterie.jpg', caption: 'Charlie et la chocolaterie' },
+      { src: '/images/JamesGrossePeche.jpg', caption: 'James et la grosse pêche' },
+      { src: '/images/LeBGG.jpg', caption: 'Le bon gros géant' },
+      { src: '/images/LeDoigtMagique.jpg', caption: 'Le doigt magique' },
+      { src: '/images/LesDeuxGredins.jpg', caption: 'Les deux gredins' },
+      { src: '/images/MaitreRenard.jpg', caption: 'L\'incroyable maître renard' },
+      { src: '/images/Matilda.jpg', caption: 'Matilda' },
+      { src: '/images/MoiBoy.jpg', caption: 'Moi, Boy' },
+      { src: '/images/PotionMagique.jpg', caption: 'George et la potion magique' },
+      { src: '/images/SacreesSorcieres.jpg', caption: 'Sacrées Sorcières' }
+  ];
+  
+  res.render('gallery', { images: images });
+});
+
 // Route pour afficher le formulaire de contact
 app.get('/contact', (req, res) => {
   res.render('pages/contact', { 
