a727383e65617f8d4e5a452bfd613c7ecbb19931
Ophélia <ochheak@gmail.com>
2025-03-25 16:30:18 +0100

    change library.sql

--- library.sql
+++ library.sql
@@ -1,38 +1,43 @@
--- Création de la table des utilisateurs
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    role VARCHAR(50) NOT NULL DEFAULT 'user' CHECK (role IN ('admin', 'user')),
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    registration_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    is_active BOOLEAN DEFAULT TRUE, -- savoir si un compte est actif
-    is_verified BOOLEAN DEFAULT FALSE -- vérifier l'authentification de l'utilisateur
+CREATE TABLE users(
+   id INT,
+   role VARCHAR(50) NOT NULL,
+   name VARCHAR(100) NOT NULL,
+   email VARCHAR(100) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   registration_date DATETIME,
+   is_active LOGICAL,
+   is_verified LOGICAL,
+   created_at DATETIME,
+   updated_at DATETIME,
+   PRIMARY KEY(id),
+   UNIQUE(email)
 );
 
--- Création de la table des catégories
-CREATE TABLE IF NOT EXISTS category (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
+CREATE TABLE category(
+   id INT,
+   name VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id),
+   UNIQUE(name)
 );
 
--- Création de la table des livres
-CREATE TABLE IF NOT EXISTS books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(100) NOT NULL,
-    description TEXT,
-    category_id INT NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE RESTRICT ON UPDATE CASCADE
+CREATE TABLE books(
+   id INT,
+   title VARCHAR(255) NOT NULL,
+   author VARCHAR(100) NOT NULL,
+   description TEXT,
+   year_publication INT,
+   img_path VARCHAR(255),
+   id_1 INT NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_1) REFERENCES category(id)
 );
 
--- Création de la table des commentaires (avec clé primaire composite)
-CREATE TABLE IF NOT EXISTS comment (
-    user_id INT NOT NULL,
-    book_id INT NOT NULL,
-    comment_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    text TEXT NOT NULL,
-    PRIMARY KEY (user_id, book_id, comment_date),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE comment(
+   id INT,
+   id_1 INT,
+   text TEXT NOT NULL,
+   comment_date DATETIME,
+   PRIMARY KEY(id, id_1),
+   FOREIGN KEY(id) REFERENCES users(id),
+   FOREIGN KEY(id_1) REFERENCES books(id)
 );
